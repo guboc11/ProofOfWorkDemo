7b62a83c55cb7a22a8dc79d51d611664d98c8bc0
guboc11 <guboc11@gmail.com>
2024-09-09 21:20:07 +0900

    Add feature rendering 32bit hash numbers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
+import Hashes from 'jshashes';
 
 function App() {
-  // const [block, setBlock] = useState({height:1})
-  const [numbers, setNumbers] = useState([]);
+  var CRC32 = require("crc-32");
+
+  const [hashNumbers, setHashNumbers] = useState([]);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setNumbers(prevNumbers => [...prevNumbers, prevNumbers.length + 1]);
+      var SHA1 = new Hashes.SHA1
+      setHashNumbers(
+        (prevHashNumbers) => {
+          var str = "1" + prevHashNumbers.length
+          
+          var hashNumberHexString = SHA1.hex(str);
+          console.log("hash number string", hashNumberHexString)
+          var hashNumber32bit = Number("0x"+hashNumberHexString) % 0xFFFFFFFF;
+          console.log("hash number 32 bit", hashNumber32bit)
+          var hashNumber32bitHexString = "0x" + hashNumber32bit.toString(16).padStart(8, '0')
+          console.log("hash number 32 bit hex string", hashNumber32bitHexString)
+
+
+          return [...prevHashNumbers, hashNumber32bitHexString]
+      });
+      console.log(" ")
     }, 1000);
 
     return () => clearInterval(interval);
@@ -17,7 +34,7 @@ function App() {
 
   return (
     <div>
-    {numbers.map((number, index) => (
+    {hashNumbers.map((number, index) => (
       <div key={index}>{number}</div>
     ))}
   </div>
