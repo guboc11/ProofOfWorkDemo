f111d4dfae3375e8cfaa8120f9d0387ed1870748
guboc11 <guboc11@gmail.com>
2024-09-09 20:24:06 +0900

    add : every 1 sec add number and print

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  // const [block, setBlock] = useState({height:1})
+  const [numbers, setNumbers] = useState([]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setNumbers(prevNumbers => [...prevNumbers, prevNumbers.length + 1]);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+
   return (
     <div>
-      <h className="text-2xl">hello blockblock</h>
-    </div>
+    {numbers.map((number, index) => (
+      <div key={index}>{number}</div>
+    ))}
+  </div>
   );
 }
 
